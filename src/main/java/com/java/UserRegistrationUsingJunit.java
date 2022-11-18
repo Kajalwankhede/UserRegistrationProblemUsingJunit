3783114b235f964d8b8c8496bbf2df82eed044c1
Kajalwankhede <kajuwankhede1510@gmail.com>
2022-11-18 14:18:15 +0530

    UC1- User need to valid first name start witch one caps and has minimum 3 characters

--- src/main/java/com/java/UserRegistrationUsingJunit.java
+++ src/main/java/com/java/UserRegistrationUsingJunit.java
@@ -3,7 +3,22 @@ import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 public class UserRegistrationUsingJunit {
-
-
+    static ArrayList<String> list = new ArrayList<String>();
+    public static boolean isValidFirstName(String firstName) {
+        System.out.println("Checking Valid First Name:");
+        list.add("Sunny");
+        list.add("Jonny");
+        String regex = "[A-Z]{1}[a-z]{3,}";
+        Pattern pattern = Pattern.compile(regex);
+        Matcher matcher = pattern.matcher(firstName);
+        boolean result =(matcher.matches());
+        if (result==true){
+            list.add("Valid name"+firstName);
+        }
+        else {
+            list.add("invalid name");
+        }
+        return result;
+    }
 
 }
